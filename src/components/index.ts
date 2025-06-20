68703ab325ab282b22303f6211d709573b70bd9b
Riz <andyrizzuto@gmail.com>
2025-06-20 00:49:31 -0500

    reset before setting claude loose

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as ScrabbleScorer } from './ScrabbleScorer';
-export { default as GameSetup } from './GameSetup';
-export { default as ScoreDisplay } from './ScoreDisplay';
-export { default as TurnManager } from './TurnManager';
-export { default as WordInput } from './WordInput';
-export { default as LetterTiles } from './LetterTiles';
-export { default as MultiWordTurn } from './MultiWordTurn';
-
